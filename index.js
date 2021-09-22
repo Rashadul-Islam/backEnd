d8f6225a6db79f0692a24e646875d71f753bd9f5
Anamulbiazid <rashadulislam4581@gmail.com>
2021-09-22 11:30:40 +0600

    user

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ldkov.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
